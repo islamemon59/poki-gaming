95b765a1d1ae5cbf464d6dcafff340571d365282
islamemon59 <islamemon59@gmail.com>
2025-10-08 20:20:06 +0600

    thumbnail bug updated

--- src/Components/Games/GameCard/GameCard.jsx
+++ src/Components/Games/GameCard/GameCard.jsx
@@ -14,24 +14,12 @@ const GameCard = ({ game, setHovered, hovered, index }) => {
         <img
           src={game?.thumbnail}
           alt={game?.title}
-          className={`w-full object-cover transition duration-300 ${
-            hovered === index ? "opacity-0" : "opacity-100"
-          }`}
+          className={`w-full object-cover transition duration-300`}
         />
-
-        {/* Preview Video on Hover */}
-        {hovered === index && (
-          <iframe
-            src={game?.previewVideo}
-            className="absolute top-0 left-0 w-full h-full"
-            frameBorder="0"
-            allow="autoplay; fullscreen"
-          ></iframe>
-        )}
 
         {/* Game Title with animation */}
         <div
-          className={`absolute bottom-3 left-0 w-full font-bold bg-opacity-60 text-white text-[16px] p-2 text-center transform transition-transform duration-500
+          className={`absolute bottom-3 left-0 w-full font-bold bg-opacity-60 text-white text-[18px] p-2 text-center transform transition-transform duration-500
           ${hovered === index ? "translate-y-0" : "translate-y-full"}`}
         >
           {game?.title}
